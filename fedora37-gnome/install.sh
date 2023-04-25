d875813e45190e3e319837f6de774a1f15072064
columbus42 <46508712+columbus42@users.noreply.github.com>
2023-04-25 18:57:43 -0400

    Update install.sh

--- fedora37-gnome/install.sh
+++ fedora37-gnome/install.sh
@@ -16,35 +16,10 @@ sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x
 sudo dnf upgrade && sudo dnf update
 
 # Install Programs
-sudo dnf install -y sublime-text
-sudo dnf install -y vlc
-sudo dnf install -y audacity
-sudo dnf install -y terminator
-sudo dnf install -y emacs
-sudo dnf install -y transmission
-sudo dnf install -y firefox
-sudo dnf install -y chromium
-sudo dnf install -y google-chrome-stable
-sudo dnf install -y lpf-spotify-client
-sudo dnf install -y gimp
-sudo dnf install -y handbrake
-sudo dnf install -y htop
-sudo dnf install -y neofetch
+sudo dnf install -y sublime-text vlc audacity terminator emacs transmission firefox chromium google-chrome-stable lpf-spotify-client gimp handbrake htop neofetch filezilla gparted fastfetch VirtualBox gnome-tweak-tool nemo papirus-icon-theme gnome-extensions-app file-roller grub-customizer breeze-cursor-theme libreoffice inkscape
+
 sudo dnf install -y p7zip-*
 sudo dnf install -y ffmpeg --allowerasing
-sudo dnf install -y filezilla
-sudo dnf install -y gparted
-sudo dnf install -y neofetch
-sudo dnf install -y VirtualBox
-sudo dnf install -y gnome-tweak-tool
-sudo dnf install -y nemo
-sudo dnf install -y papirus-icon-theme
-sudo dnf install -y gnome-extensions-app
-sudo dnf install -y file-roller
-sudo dnf install -y grub-customizer
-sudo dnf install -y breeze-cursor-theme
-sudo dnf install -y libreoffice
-sudo dnf install -y inkscape
 
 # Remove software
 sudo dnf remove -y nautilus
